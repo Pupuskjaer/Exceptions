1a182fa66c56274cf4a7455fde1c02acccc683ed
KhasanovTR <khasanovtimur2001@gmail.com>
2024-02-11 00:06:43 +0400

    Обработка исключений

--- src/WrongLoginException.java
+++ src/WrongLoginException.java
@@ -1,5 +1,5 @@
 public class WrongLoginException extends RuntimeException{
-    public WrongLoginException() {
-        super();
+    public WrongLoginException(String message) {
+        super(message);
     }
 }
